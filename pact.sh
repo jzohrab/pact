08095d6ffe345064f3eedde22c36e25dd20d771e
Jeff Zohrab <jzohrab@gmail.com>
2022-05-12 22:01:36 -0700

    Startup script feedback.

--- pact.sh
+++ pact.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
+echo "Creating environment ..."
 python3 -m venv .venv
 source .venv/bin/activate
-echo Running pact in $VIRTUAL_ENV
 
+echo "Running pact in $VIRTUAL_ENV ..."
 python main.py
 
 deactivate
