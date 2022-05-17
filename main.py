1240bf6d87cf4c5b22aec0e4eb245f4ac8859d1e
Jeff Zohrab <jzohrab@gmail.com>
2022-05-17 11:16:44 -0700

    Add -d option for start directory of main.py, useful for scripts.

--- main.py
+++ main.py
@@ -24,8 +24,20 @@ def get_config():
     return config
 
 
+def actual_filename(f, optionaldir = None):
+    if os.path.exists(f):
+        return f
+    ret = os.path.join(optionaldir, f)
+    if os.path.exists(ret):
+        return ret
+    # Just return the original file, it's later checked with
+    # must_exist.
+    return f
+
+
 import argparse
 parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dirname", help="directory to search for files")
 parser.add_argument("-p", "--pact", help="path to .pact file")
 parser.add_argument("-m", "--mp3", help="path to .mp3 file")
 args = parser.parse_args()
@@ -35,11 +47,13 @@ args = parser.parse_args()
 setup = None
 
 if args.pact is not None:
-    must_exist(args.pact)
-    setup = lambda app: app.load_pact_file(args.pact)
+    f = actual_filename(args.pact, args.dirname)
+    must_exist(f)
+    setup = lambda app: app.load_pact_file(f)
 elif args.mp3 is not None:
-    must_exist(args.mp3)
-    setup = lambda app: app.load_mp3(args.mp3)
+    f = actual_filename(args.mp3, args.dirname)
+    must_exist(f)
+    setup = lambda app: app.load_mp3(f)
 else:
     # Default
     s = "pact/assets/Python audio clip tool.mp3"
