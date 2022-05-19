1f0f2a6ec59236db7d69a6c9c27c859be6cfa5ac
Jeff Zohrab <jzohrab@gmail.com>
2022-05-18 22:32:35 -0700

    Version bump, 0.2.0.

--- pact/_version.py
+++ pact/_version.py
@@ -1,1 +1,35 @@
-__version__ = "0.1.2"
+__version__ = "0.2.0"
+
+# Get version changes via
+# git log -- pact/_version.py
+
+version_notes = """
+0.2.0 (b699ad8): Clip notes
+
+- allow lookup from notes textbox
+- add notes textbox to clip window
+
+0.1.2 (5276a01): Clip list display changes
+
+0.1.1 (2e29895): Many small changes:
+
+- export checkmarks
+- main.py flags
+- docs
+- more tests
+- lookup modules
+- autosave
+- bugfixes
+
+0.1.0 (2d9dc14): Initial stable version:
+
+- session files
+- using transcriptions
+- VLC and ffmpeg
+- main app
+
+
+0.0.0 (dfeb216): Initial commit
+
+- copy of files over from another test project.
+"""
