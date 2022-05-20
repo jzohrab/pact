79f22da095c2248cd9bdd6ba9b1810aa5d9b469b
Jeff Zohrab <jzohrab@gmail.com>
2022-05-19 22:51:21 -0700

    Version bump, 0.2.1

--- pact/_version.py
+++ pact/_version.py
@@ -1,9 +1,13 @@
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 # Get version changes via
 # git log -- pact/_version.py
 
 version_notes = """
+0.2.1 (4af3f26): Auto-create temp pact file
+
+- opening mp3 creates a temp pact file, just in case.
+
 0.2.0 (b699ad8): Clip notes
 
 - allow lookup from notes textbox
